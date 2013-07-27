e7eb31da47fe6d7d38fe5a0afaade3536361e23f
dubuapt <kozazz@hanmail.net>
2013-07-27 10:32:08 +0900

    remove key

--- src/com/parse/demo/ToDoListApplication.java
+++ src/com/parse/demo/ToDoListApplication.java
@@ -12,7 +12,7 @@ public class ToDoListApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		Parse.initialize(this, "jIMnY07BUyA2xgkCjmnXqAvEDPjtBpDoLqoS2qCF", "Y1OHobydfhRtD23n8RXc30nzFSKrb83Y4F2oUAP1");
+		Parse.initialize(this, "", "");
 		ParseUser.enableAutomaticUser();
 		ParseACL defaultACL = new ParseACL();
 		// Optionally enable public read access.
