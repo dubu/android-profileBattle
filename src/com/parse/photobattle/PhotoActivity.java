b8991c691b3a89762a8ba262a01367993f4ca8cc
dubuapt <kozazz@hanmail.net>
2013-07-28 21:01:05 +0900

    프사 배틀 ㅋㅋ

---
+++ src/com/parse/photobattle/PhotoActivity.java
@@ -0,0 +1,9 @@
+package com.parse.photobattle;
+
+/**
+ * User: kingkingdubu
+ * Date: 13. 7. 28
+ * Time: 오후 5:54
+ */
+public class PhotoActivity {
+}
---
+++ src/com/parse/photobattle/ToDoListApplication.java
@@ -0,0 +1,20 @@
+package com.parse.photobattle;
+
+import com.parse.*;
+
+import android.app.Application;
+
+public class ToDoListApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Parse.initialize(this, "jIMnY07BUyA2xgkCjmnXqAvEDPjtBpDoLqoS2qCF", "Y1OHobydfhRtD23n8RXc30nzFSKrb83Y4F2oUAP1");
+		ParseUser.enableAutomaticUser();
+		ParseACL defaultACL = new ParseACL();
+		// Optionally enable public read access.
+		 defaultACL.setPublicReadAccess(true);
+		ParseACL.setDefaultACL(defaultACL, true);
+
+	}
+}
